45bd2b5628a5d0a0ca2391858a5f95aebc7391bb
Jonathan Payne <jdapayne@gmail.com>
2020-11-23 13:54:43 +0000

    Start trapezium data

---
+++ modules/Question/GraphicQ/AreaPerimeter/TrapeziumAreaData.ts
@@ -0,0 +1,14 @@
+/*
+import { Value } from "./RectangleAreaData"
+
+export default class TrapeziumAreaData {
+  a: Value // two parallel sides
+  b: Value //  "   ""
+  height: Value
+  side1: Value // Slanted sides
+  side2: Value
+  private readonly dp: number = 0
+  private _area: Value
+  private _perimeter: Value
+}
+*/
